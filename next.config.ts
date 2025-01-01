34604d103b8dbd5f61e8799500ad2f861178f257
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2025-01-01 23:25:04 +0200

    Next config file update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // Enable static export
+  reactStrictMode: true, // Optional: Keeps React in strict mode
+  images: {
+    unoptimized: true, // Disable image optimization for static exports
+  },
 };
 
 export default nextConfig;
