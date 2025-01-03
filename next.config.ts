3442f3a745ad692e3f7c03aec2a2f474446aae50
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2025-01-03 10:39:03 +0200

    Config file changed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Enables static export
-  reactStrictMode: true,
+  experimental: {
+    serverActions: true, // Enable Server Actions if needed
+  },
   images: {
     unoptimized: true, // Required for static export if you're using the Next.js Image component
   },
